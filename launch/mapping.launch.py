34b71df1807f2853ac7ba23e6db4fe35c4286280
ConeRobo <skihatduckie@gmail.com>
2024-06-20 14:40:56 -0400

    Add `mapping.yaml`

--- launch/mapping.launch.py
+++ launch/mapping.launch.py
@@ -4,7 +4,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
