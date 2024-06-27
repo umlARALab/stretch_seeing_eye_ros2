ba3b0082f17d69f813d3479bdb2af8a5d11f313b
ConeRobo <skihatduckie@gmail.com>
2024-06-27 15:31:19 -0400

    Add nav_driver params file

--- launch/navigation_driver.launch.py
+++ launch/navigation_driver.launch.py
@@ -3,7 +3,7 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    """Launch the stretch2 physical robot, a simulaton of the stretch2,
+    """Launch the physical stretch2 robot, a simulaton of the stretch2,
        or a simulation of another robot remapped to stretch2 for navigation."""
     return LaunchDescription([
 
