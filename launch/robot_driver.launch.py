cfca8f29bc0b34a0324d8b5da7b47657273b04c0
ConeRobo <skihatduckie@gmail.com>
2024-07-03 16:00:46 -0400

    Update filenames

--- launch/robot_driver.launch.py
+++ launch/robot_driver.launch.py
@@ -6,8 +6,8 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package="stretch_seeing_eye_ros2",
-            executable="nav_driver",
-            name="nav_driver",
+            executable="robot_driver",
+            name="robot_driver",
             output="screen"
         )
     ])
