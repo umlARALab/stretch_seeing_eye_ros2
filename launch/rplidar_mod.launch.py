6ed2b96183dc2a8c63158ba87df9b3485deeb584
UML ARA Lab <141755495+umlARALab@users.noreply.github.com>
2024-07-18 14:42:55 -0400

    9 convert rplidar modlaunch to ros2 (#25)
    * Add rplidar_mod.launch.py
    
    * Migrate rplidar_mod.launch to ROS2
    
    Co-authored-by: SkiHatDuckie <SkiHatDuckie@gmail.com>
    
    ---------
    Co-authored-by: SkiHatDuckie <SkiHatDuckie@gmail.com>

---
+++ launch/rplidar_mod.launch.py
@@ -0,0 +1,35 @@
+import os
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    scan_filter_params = os.path.join(
+        get_package_share_directory("stretch_seeing_eye_ros2"),
+        "config", "rplidar_mod.yaml"
+    )
+    return LaunchDescription([
+        Node(
+            package="rplidar_ros",
+            executable="rplidar_node",
+            name="lidar_node",
+            output="screen",
+            parameters=[
+                {"serial_port": "/dev/hello-lrf"},
+                {"serial_baudrate": 115200},
+                {"frame_id": "laser"},
+                {"inverted": False},
+                {"angle_compensate": True},
+                {"scan_mode": "Boost"}
+            ]
+        ),
+        Node(
+            package="laser_filters",
+            executable="scan_to_scan_filter_chain",
+            name="laser_filter",
+            namespace="scan_filter_chain",
+            parameters=[scan_filter_params]
+        )
+    ])
